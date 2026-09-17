25b548aea4ba8a8535b589a47cbdfe58b2a44aab
Pamella <pamella.alvesaandradee@gmail.com>
2026-09-16 21:29:36 -0300

    Criação da barra de navegação, ainda com erros

--- app/src/main/java/com/example/hemo_conecta/MainActivity.kt
+++ app/src/main/java/com/example/hemo_conecta/MainActivity.kt
@@ -5,16 +5,56 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom)
             insets
+        }
+
+        // NavController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                    as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        // Bottom Navigation
+        val bottomNavigation =
+            findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        bottomNavigation.setupWithNavController(navController)
+
+        // observa em que tela o usuário está para controlar quando a barra aparece
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+                R.id.splashFragment,
+                R.id.loginFragment,
+                R.id.cadastroFragment -> {
+                    bottomNavigation.visibility = BottomNavigationView.GONE
+                }
+
+                else -> {
+                    bottomNavigation.visibility = BottomNavigationView.VISIBLE
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/hemo_conecta/ui/InicioFragment.kt
+++ app/src/main/java/com/example/hemo_conecta/ui/InicioFragment.kt
@@ -1,19 +1,32 @@
 package com.example.hemo_conecta.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.hemo_conecta.R
-
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.hemo_conecta.databinding.FragmentInicioBinding
 
 class InicioFragment : Fragment() {
+
+    private var _binding: FragmentInicioBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
+    ): View {
+        _binding = FragmentInicioBinding.inflate(inflater, container, false)
+
+        val navController = findNavController()
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+}
